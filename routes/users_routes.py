df921f1e3fe8ff8a0fe9adafa5b9704e78b654b9
SPedemonte <santiagopedemonte02@gmail.com>
2025-09-17 19:59:39 -0300

    refactor endpoint name

--- routes/users_routes.py
+++ routes/users_routes.py
@@ -12,16 +12,16 @@ async def get_all_users(
 ):
     return user_service.get_all_users()
 
-@users_router.patch("/{email}/")
+@users_router.get("/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+@users_router.patch("/me/")
 async def update_user(
     user: UserUpdate,
     current_user: Annotated[User, Depends(get_current_active_user)],
     user_service: UserService = Depends(get_user_service),
 ):
     return user_service.update_user(current_user, user)
-
-@users_router.get("/me/", response_model=User)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-):
-    return current_user
